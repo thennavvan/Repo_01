e6b3f984f4cb820e7b6cb9017f87446b53174092
JoeMarian <=joemarian3010@gmail.com>
2026-01-23 04:19:32 +0000

    added code

---
+++ code.java
@@ -0,0 +1,27 @@
+package com.hsbc.app;
+
+import com.hsbc.beans.Person;
+import com.hsbc.beans.Supervisor;
+
+public class PersonDriver {
+    public static void main(String[] args) {
+        Person p1 = new Person(101, "Sanjjaaaiii", "sanj@nothing.com");
+        /*p1.setValues(101, "Sanjai", "msanjai@gmail.com");*/
+        //p1.dispPerson();
+        System.out.println("p1; "+ p1.toString());
+        p1 = new Person(102, "thala", "thalapathy@gmail.com");
+        /*p1.setValues(102, "hola", "olaolaoal@yahoo.com");*/
+        System.out.println("p1; "+ p1.toString());
+
+        Person p2 = new Person(107, "abcd", "efgh@g.com");
+        System.out.println("p2; "+ p2.toString());
+
+        Supervisor s1 = new Supervisor(102,"rkpk", "sample@gmail.com", 15);
+        System.out.println("Calling display wuth p1: ");
+        p1.display();
+
+        System.out.println("Calling display s1");
+        s1.display();
+    }
+}
+
