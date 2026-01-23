725e0dc6cad3b3e0e40b01387b5286fd8208558d
JoeMarian <=joemarian3010@gmail.com>
2026-01-23 05:30:07 +0000

    added code3

--- code.java
+++ code.java
@@ -1,3 +1,4 @@
+person.java
 package com.hsbc.app;
 
 import com.hsbc.beans.Manager;
@@ -30,3 +31,92 @@ public class PersonDriver {
     }
 }
 
+_______________________________________________________________
+persondriver.java
+package com.hsbc.app;
+
+import com.hsbc.beans.Manager;
+import com.hsbc.beans.Person;
+import com.hsbc.beans.Supervisor;
+
+public class PersonDriver {
+    public static void main(String[] args) {
+        Person p1 = new Person(101, "Sanjjaaaiii", "sanj@nothing.com");
+        /*p1.setValues(101, "Sanjai", "msanjai@gmail.com");*/
+        //p1.dispPerson();
+        System.out.println("p1; "+ p1.toString());
+        p1 = new Person(102, "thala", "thalapathy@gmail.com");
+        /*p1.setValues(102, "hola", "olaolaoal@yahoo.com");*/
+        System.out.println("p1; "+ p1.toString());
+
+        Person p2 = new Person(107, "abcd", "efgh@g.com");
+        System.out.println("p2; "+ p2.toString());
+
+        Supervisor s1 = new Supervisor(102,"rkpk", "sample@gmail.com", 15);
+        System.out.println("Calling display wuth p1: ");
+        p1.display();
+
+        System.out.println("Calling display s1");
+        s1.display();
+
+        Manager m1 = new Manager(108, "Rutu","rutu@gmail.com", 18);
+        System.out.println("Calling manager:");
+        m1.display();
+    }
+}
+
+_______________________________________________________
+manager.java
+package com.hsbc.beans;
+
+public class Manager extends Person{
+    int qualification;
+    public Manager(){
+        super();
+    }
+    public Manager( int id, String name, String email, int qualification){
+        super (id, name, email);
+        this.qualification=qualification;
+    }
+
+    public int getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(int qualification) {
+        this.qualification = qualification;
+    }
+
+    @Override
+    public void display() {
+        super.display();
+        System.out.println("qualification: "+qualification);
+    }
+}
+______________________________________________________________
+supervisor.java
+package com.hsbc.beans;
+
+public class Supervisor extends Person{
+    int experience;
+
+    public Supervisor(){
+        super();
+        System.out.println("abcd");
+    }
+    public Supervisor(int id, String name, String email, int experience){
+        super (id, name, email);
+        this.experience=experience;
+    }
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public void display(){
+        System.out.println("experience: "+experience);
+    }
+}
